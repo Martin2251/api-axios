d58e7588a6f7a41a52afcaaeb980a4acf8578a6c
martin lowe <martinlowe673@gmail.com>
2021-12-02 07:57:31 +0000

    made call

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import logo from "./logo.svg";
 import axios from "axios";
+import { useState } from "react";
 
 function App() {
+  const [quote, setQuote] = useState("");
   const getQuote = () => {
     axios
       .get("https://api.quotable.io/random")
       .then((response) => {
-        console.log(response);
+        console.log(response.data.content);
+        setQuote(response.data.content);
       })
       .catch((err) => {
         console.log(err);
       });
   };
+  // if a quoute is true it will return in paragraph tags
   return (
     <div className="App">
       <button onClick={getQuote}>Get Quote</button>
+      {quote && <p>{quote}</p>}
     </div>
   );
 }
